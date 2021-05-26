acaf1d14adf0a195e779ecd27041e3e28b32306f
Vinith Rajasekar <vinith@i3software.in>
2021-05-26 19:06:03 +0530

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "tailwindcss/tailwind.css";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const client = new ApolloClient({
+    uri: "https://graphqlzero.almansi.me/api",
+    cache: new InMemoryCache(),
+  });
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,101 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React, { useState } from "react";
+import { useQuery, useMutation, gql } from "@apollo/client";
+
+const GET_DATA = gql`
+  query {
+    post(id: 1) {
+      id
+      title
+      body
+    }
+  }
+`;
+
+const CREATE_DATA = gql`
+  mutation($input: CreatePostInput!) {
+    createPost(input: $input) {
+      id
+      title
+      body
+    }
+  }
+`;
+const UPDATE_DATA = gql`
+  mutation($id: ID!, $input: UpdatePostInput!) {
+    updatePost(id: $id, input: $input) {
+      id
+      body
+    }
+  }
+`;
+const DELETE_DATA = gql`
+  mutation($id: ID!) {
+    deletePost(id: $id)
+  }
+`;
 
 export default function Home() {
+  const [input, setInput] = useState("hello");
+  const [update, setUpdate] = useState("");
+  const [create, setCreate] = useState("");
+  const [del, setDel] = useState("");
+  const stuff = useQuery(GET_DATA);
+  const { data } = stuff;
+  const [createData] = useMutation(CREATE_DATA);
+  const [updateData] = useMutation(UPDATE_DATA);
+  const [deleteData] = useMutation(DELETE_DATA);
+
+  async function handleUpdateData() {
+    const { data } = await updateData({
+      variables: {
+        id: 1,
+        input: {
+          body: "Some updated content.",
+        },
+      },
+      refetchQueries: [{ query: GET_DATA }],
+    });
+    setUpdate(data.updatePost);
+    console.log(data.updatePost);
+  }
+
+  async function handleCreateData() {
+    const { data } = await createData({
+      variables: {
+        input: {
+          title: input,
+          body: "Some interesting content.",
+        },
+      },
+      refetchQueries: [{ query: GET_DATA }],
+    });
+    setCreate(data.createPost);
+    console.log(data.createPost);
+  }
+  async function handleDeleteData() {
+    const { data } = await deleteData({
+      variables: { id: 10 },
+      refetchQueries: [{ query: GET_DATA }],
+    });
+    setDel(data.deletePost);
+    console.log(data);
+  }
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+    <div>
+      <button onClick={handleCreateData}>Create</button>
+      <button onClick={handleUpdateData}>Update</button>
+      <button onClick={handleDeleteData}>Delete</button>
+      <div className="mt-20">
+        <h1>{update.id}</h1>
+        <p>{update.body}</p>
+      </div>
+      <div className="mt-20">
+        <h1>{create.id}</h1>
+        <h1>{create.title}</h1>
+        <p>{create.body}</p>
+      </div>
+      <div>{del ? <p>Sucessfully deleted!</p> : null}</div>
     </div>
-  )
+  );
 }
